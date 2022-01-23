3e8a63cc32d8a31a69de757b34169f974c6a12ae
nerfgrv <54734399+nerfgrv@users.noreply.github.com>
2022-01-23 22:00:10 +0530

    Time: 24 ms (88.55%), Space: 10.7 MB (70.04%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> hash(26);
+        for(int i=0;i<s.length();i++){
+            hash[s[i]-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(hash[s[i]-'a']==1) return i;
+        }
+        return -1;
+    }
+};
