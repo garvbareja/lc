ba63b6acfdc2811aab9e03c79952571f257d8b3d
nerfgrv <54734399+nerfgrv@users.noreply.github.com>
2022-01-23 15:27:19 +0530

    Time: 2 ms (21.79%), Space: 6.2 MB (76.43%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -2,9 +2,17 @@ class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
         vector<int> ans;
-        int arr[]={1,2,3,4,5,6,7,8,9,12,23,34,45,56,67,78,89,123,234,345,456,567,678,789,1234,2345,3456,4567,5678,6789,12345,23456,34567,45678,56789,123456,234567,345678,456789,1234567,2345678,3456789,12345678,23456789,123456789};
-        for(int i=0;i<45;i++){
-            if(arr[i]>=low && arr[i]<=high) ans.push_back(arr[i]);
+        queue<int> q;
+        for(int i=1;i<=9;i++){
+            q.push(i);
+        }
+        while(!q.empty()){
+                int front=q.front(); q.pop();
+                if(front>=low && front<=high) ans.push_back(front);
+            if(front%10 < 9){
+                int newnumber = front*10 + front%10 + 1;
+                if(newnumber<=high) q.push(newnumber);
+            }
         }
         return ans;
     }
