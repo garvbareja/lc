541cc5201382a40f9ae4b02ecd181085795e6595
nerfgrv <54734399+nerfgrv@users.noreply.github.com>
2022-01-23 21:53:26 +0530

    Time: 17 ms (29.64%), Space: 11.8 MB (90.02%) - LeetHub

---
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool digit(string& s){
+        if(s.length()==1 && !isdigit(s[0])) return false;
+        return true;
+    }
+    
+    int todigit(string& s){
+        int ret=0;bool flag=false;
+        int i=0; if(s[i]=='-'){
+            flag=true;
+            i++;
+        }
+        for(;i<s.length();i++){
+            ret*=10;
+            ret+=(s[i]-'0');
+        }
+        if(flag) return -ret;
+        return ret;
+    }
+    
+    int evalRPN(vector<string>& tokens) {
+        stack<int> s;
+        int i=0;
+        while(i<tokens.size()){
+            if(digit(tokens[i])){
+                s.push(todigit(tokens[i]));
+            }else{
+                int x=s.top(); s.pop();
+                int y=s.top(); s.pop();
+                if(tokens[i]=="+"){
+                    y+=x;
+                }else if(tokens[i]=="-"){
+                    y-=x;
+                }else if(tokens[i]=="*"){
+                    y*=x;
+                }else{
+                    y/=x;
+                }
+                s.push(y);
+            }
+            i++;
+        }
+        return s.top();
+    }
+};
