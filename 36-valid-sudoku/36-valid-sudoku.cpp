9a9c4a0ca0a51940d5e1a68d23bd96c1a70d9920
nerfgrv <54734399+nerfgrv@users.noreply.github.com>
2022-01-25 03:37:36 +0530

    Time: 32 ms (41.41%), Space: 17.9 MB (97.07%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int row[9][9]={0},col[9][9]={0},sq[3][3][9]={0};
+        for(int i=0;i<9;i++)
+            for(int j=0;j<9;j++)
+                if(isdigit(board[i][j])){
+                    int num=board[i][j]-'1';
+                    if(row[i][num]++ || col[j][num]++ || sq[i/3][j/3][num]++) return false;
+                }
+        return true;
+    }
+};
