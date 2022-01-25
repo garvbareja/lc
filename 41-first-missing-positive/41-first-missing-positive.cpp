6b055254e547c96f3f75a61a995e3d8897e736cd
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-01-25 15:44:09 +0530

    Time: 335 ms (17.21%), Space: 83 MB (78.34%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
+    bool valid(int i,int high){
+        if(i<=0 || i>=high) return false;
+        return true;
+    }
+    
     int firstMissingPositive(vector<int>& nums) {
-        unordered_set<int> hash;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]>0){
-                hash.insert(nums[i]);
+            if(!valid(nums[i],nums.size()+1)) nums[i]=0;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(abs(nums[i])>0){
+                if(!nums[abs(nums[i])-1]) nums[abs(nums[i])-1]=-abs(nums[i]);
+                else nums[abs(nums[i])-1]=-abs(nums[abs(nums[i])-1]);
             }
         }
-        for(int i=1;i<=nums.size()+1;i++){
-            if(!hash.count(i)) return i;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>=0) return i+1;
         }
-        return 1;
+        return nums.size()+1;
     }
 };
