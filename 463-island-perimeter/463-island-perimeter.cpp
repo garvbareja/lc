198701bb8ed792040da19065db2c0998e7bd8e34
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-01-25 21:50:54 +0530

    Time: 84 ms (91.99%), Space: 96.1 MB (83.83%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.cpp
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -1,35 +1,16 @@
 class Solution {
 public:
-    vector<vector<bool>> vis;
-    int perimeter=0;
-    
-    bool valid(vector<vector<int>>& grid,int i,int j){
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[i].size() || !grid[i][j]) return false;
-        return true;
-    }
-    
-    void dfs(vector<vector<int>>& grid,int i,int j){
-        vis[i][j]=true;
-        vector<int> x={-1,1,0,0};
-        vector<int> y={0,0,1,-1};
-        for(int z=0;z<4;z++){
-            if(valid(grid,i+x[z],j+y[z])){
-                if(!vis[i+x[z]][j+y[z]]) dfs(grid,i+x[z],j+y[z]);
-            }
-            else perimeter++;
-        }
-    }
-    
-    int islandPerimeter(vector<vector<int>>& grid){
-        vis.resize(grid.size(),vector<bool> (grid[0].size()));
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int count=0,repeat=0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[i].size();j++){
                 if(grid[i][j]){
-                    dfs(grid,i,j);
-                    return perimeter;
+                    count++;
+                    if(i!=0 && grid[i-1][j]) repeat++;
+                    if(j!=0 && grid[i][j-1]) repeat++;
                 }
             }
         }
-        return 0;
+        return (4*count)-(2*repeat);
     }
 };
