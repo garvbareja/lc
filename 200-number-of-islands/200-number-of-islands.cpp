7c3d36fc55ac146a6684f26b65124cafc2ae4544
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-01-25 16:33:03 +0530

    Time: 83 ms (13.90%), Space: 18.8 MB (12.12%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid,int i,int j){
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[i].size() || grid[i][j]=='0') return;
-        grid[i][j]='0';
-        vector<int> x={-1,1,0,0};
-        vector<int> y={0,0,-1,1};
-        for(int z=0;z<4;z++) dfs(grid,i+x[z],j+y[z]);
+    bool valid(vector<vector<char>>& grid,int i,int j){
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[i].size() || grid[i][j]=='0') return false;
+        return true;
     }
     
-    int numIslands(vector<vector<char>>& grid){
+    void bfs(vector<vector<char>>& grid,int i,int j){
+        queue<pair<int,int>> q; q.push({i,j}); grid[i][j]='0';
+        vector<int> x={1,-1,0,0};
+        vector<int> y={0,0,1,-1};
+        while(!q.empty()){
+            int a=q.front().first; int b=q.front().second; q.pop();
+            for(int z=0;z<4;z++){
+                if(valid(grid,a+x[z],b+y[z])){
+                    grid[a+x[z]][b+y[z]]='0';
+                    q.push({a+x[z],b+y[z]});
+                }
+            }
+        }
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
         int islands=0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[i].size();j++){
                 if(grid[i][j]=='1'){
-                    dfs(grid,i,j);
+                    bfs(grid,i,j);
                     islands++;
                 }
             }
