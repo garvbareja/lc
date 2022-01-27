154accade5cfff11ab89b6c663cdb5bdae82f014
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-01-28 04:04:38 +0530

    Time: 3 ms (25.42%), Space: 6.4 MB (9.83%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[') stk.push(s[i]);
+            else{
+                if(stk.empty()) return false;
+                if(s[i]==')'){if(stk.top()!='(') return false; stk.pop();}
+                if(s[i]=='}'){if(stk.top()!='{') return false; stk.pop();}
+                if(s[i]==']'){if(stk.top()!='[') return false; stk.pop();}
+            }
+        }
+        return stk.empty();
+    }
+};
