ff269f90276811ad44355429b6c11f297ff009e2
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-01-29 14:19:52 +0530

    Time: 152 ms (51.00%), Space: 64 MB (48.31%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& nums) {
-        vector<int> nsr(nums.size()),nsl(nums.size());
-        stack<pair<int,int>> s; s.push({-1,-1});
-        for(int i=0;i<nums.size();i++){
-            while(s.top().first>=nums[i]) s.pop();
-            nsl[i]=s.top().second;
-            s.push({nums[i],i});
-        }
-        while(!s.empty()) s.pop(); s.push({-1,nums.size()});
+        vector<int> calc(nums.size());
+        stack<pair<int,int>> s; s.push({-1,nums.size()});
         for(int i=nums.size()-1;i>=0;i--){
             while(s.top().first>=nums[i]) s.pop();
-            nsr[i]=s.top().second;
+            calc[i]=s.top().second-1;
+            s.push({nums[i],i});
+        }
+        while(!s.empty()) s.pop(); s.push({-1,-1});
+        for(int i=0;i<nums.size();i++){
+            while(s.top().first>=nums[i]) s.pop();
+            calc[i]-=s.top().second;
             s.push({nums[i],i});
         }
         int ans=nums[0];
         for(int i=0;i<nums.size();i++){
-            ans=max(ans,(nsr[i]-nsl[i]-1)*nums[i]);
+            ans=max(ans,(calc[i])*nums[i]);
         }
         return ans;
     }
