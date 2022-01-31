bfecc582e972593e9c3da62dd9bad384d138f7dc
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-01-31 23:15:40 +0530

    Time: 4 ms (24.86%), Space: 7.1 MB (20.61%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -9,20 +9,25 @@ public:
         int n=0;
         vector<int> x={0,1,0,-1,-1,1,1,-1};
         vector<int> y={1,0,-1,0,1,1,-1,-1};
-        for(int z=0;z<8;z++) if(valid(nums,i+x[z],j+y[z]) && nums[i+x[z]][j+y[z]]) n++;
+        for(int z=0;z<8;z++) if(valid(nums,i+x[z],j+y[z]) && nums[i+x[z]][j+y[z]]>0) n++;
         return n;
     }
     
-    void gameOfLife(vector<vector<int>>& board) {
-        vector<vector<int>> nums=board;
+    void gameOfLife(vector<vector<int>>& nums) {
         for(int i=0;i<nums.size();i++){
             for(int j=0;j<nums[i].size();j++){
                 int n=help(nums,i,j);
                 if(nums[i][j]){
-                    if(!(n==2||n==3)) board[i][j]=0;
+                    if(!(n==2||n==3)) nums[i][j]=2;
                 }else{
-                    if(n==3) board[i][j]=1;
+                    if(n==3) nums[i][j]=-1;
                 }
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums[i].size();j++){
+                if(nums[i][j]==2) nums[i][j]=0;
+                else if(nums[i][j]==-1) nums[i][j]=1;
             }
         }
     }
