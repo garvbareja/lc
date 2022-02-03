9c0fd544cd57b24d4a388c2f2cd58cb9ac0a80c3
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-02-04 00:23:30 +0530

    Time: 677 ms (5.79%), Space: 24.4 MB (49.44%) - LeetHub

---
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4){
+        int ans=0; unordered_map<int,int> hash;
+        for(int i=0;i<nums3.size();i++)
+            for(int j=0;j<nums4.size();j++) hash[nums3[i]+nums4[j]]++;
+        for(int i=0;i<nums1.size();i++)
+            for(int j=0;j<nums2.size();j++)
+                if(hash.count(-(nums1[i]+nums2[j]))) ans+=hash[-(nums1[i]+nums2[j])];
+        return ans;
+    }
+};
