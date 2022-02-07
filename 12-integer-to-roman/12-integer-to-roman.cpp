e7b5006af252f8dafc36d9120fcd85d5de751727
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-02-07 22:19:02 +0530

    Time: 15 ms (38.12%), Space: 9.4 MB (20.69%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -3,11 +3,12 @@ public:
     string intToRoman(int num) {
         vector<pair<string,int>> map = {{"M",1000},{"CM",900},{"D",500},{"CD",400},{"C",100},{"XC",90},{"L",50},{"XL",40},{"X",10},{"IX",9},{"V",5},{"IV",4},{"I",1}};
         string res="";
-        for(int i=0;i<map.size();i++){
-            int cur_count=num/map[i].second;
-            while(cur_count--) res+=map[i].first;
-            num%=map[i].second;
-            if(!num) break;
+        int i=0;
+        while(num){
+            if(num>=map[i].second){
+                res+=map[i].first;
+                num-=map[i].second;
+            }else i++;
         }
         return res;
     }
