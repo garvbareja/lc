376eeb0b68add0aa2bd769ef9784765897820180
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-02-09 02:30:16 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (55.49%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
+    vector<vector<int>> ans;
     vector<int> temp;
-    set<vector<int>> st;
     void dfs(vector<int>& nums,int i){
-        if(i==nums.size()) return;
-            temp.push_back(nums[i]); st.insert(temp); dfs(nums,i+1); temp.pop_back();
-            dfs(nums,i+1);
+        ans.push_back(temp);
+        for(int j=i;j<nums.size();j++){
+            if(j==i || nums[j-1]!=nums[j]){
+                temp.push_back(nums[j]);
+                dfs(nums,j+1);
+                temp.pop_back();
+            }
+        }
     }
     
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+    vector<vector<int>> subsetsWithDup(vector<int>& nums){
         sort(nums.begin(),nums.end());
         dfs(nums,0);
-        vector<vector<int>> ans(st.begin(),st.end());
-        ans.push_back({});
         return ans;
     }
 };
