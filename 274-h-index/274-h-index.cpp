75e39f3cf0272514dddbcb81594994720d164839
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-02-08 23:36:46 +0530

    Time: 7 ms (21.55%), Space: 8.6 MB (78.92%) - LeetHub

---
+++ 274-h-index/274-h-index.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hIndex(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int low=0,len=nums.size(),high=len-1;
+        while(low<=high){
+             int mid = low +(high-low)/2;
+            if(nums[mid]==len-mid) return nums[mid];
+            if(nums[mid]<len-mid) low=mid+1;
+            else high=mid-1;
+        }
+        return len-low;
+    }
+};
