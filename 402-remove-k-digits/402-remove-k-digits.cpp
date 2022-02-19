dd41a8e4f5dc3cace6f988562d75e9a02aa97c59
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-02-19 13:19:28 +0530

    Time: 3 ms (91.11%), Space: 6.9 MB (87.80%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    string removeKdigits(string &num, int k) {
-        if(k==num.length()) return "0";
+    string removeKdigits(string &s, int k) {
+        if(k==s.length()) return "0";
         string ans="";
-        for(int i=0;i<num.length();i++){
+        for(int i=0;i<s.length();i++){
             if(ans.length()){
-                while(k && ans.back()>num[i]){
-                    ans.pop_back(); k--;
-                }
-                if(ans.length() || num[i]!='0') ans.push_back(num[i]);
-            }else if(num[i]!='0') ans.push_back(num[i]);
+                while(k && ans.length() && ans.back()>s[i]){ans.pop_back(); k--;}
+                if(ans.length() || s[i]!='0') ans.push_back(s[i]);
+            }else{
+                if(s[i]!='0') ans.push_back(s[i]);
+            }
         }
-        while(k-- && ans.length()) ans.pop_back();
+        while(ans.length() && k) {ans.pop_back(); k--;}
         return ans.length()?ans:"0";
     }
 };
