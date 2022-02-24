a15ac9a6ca33bd32ebaf907e26ce3bea1c1c2028
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-02-24 14:24:49 +0530

    Time: 48 ms (56.01%), Space: 12.6 MB (43.51%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
-    bool valid(vector<vector<char>>& grid,int i,int j){
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[i].size() || grid[i][j]=='0') return false;
-        return true;
-    }
+    vector<vector<bool>> vis;
+    vector<int> x={0,0,-1,1};
+    vector<int> y={1,-1,0,0};
     
-    void bfs(vector<vector<char>>& grid,int i,int j){
-        queue<pair<int,int>> q; q.push({i,j}); grid[i][j]='0';
-        vector<int> x={1,-1,0,0};
-        vector<int> y={0,0,1,-1};
-        while(!q.empty()){
-            int a=q.front().first; int b=q.front().second; q.pop();
-            for(int z=0;z<4;z++){
-                if(valid(grid,a+x[z],b+y[z])){
-                    grid[a+x[z]][b+y[z]]='0';
-                    q.push({a+x[z],b+y[z]});
-                }
-            }
-        }
+    void dfs(vector<vector<char>>& grid,int i,int j){
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[i].size() || grid[i][j]=='0' || vis[i][j]) return;
+        vis[i][j]=true;
+        for(int z=0;z<4;z++) dfs(grid,i+x[z],j+y[z]);
     }
     
     int numIslands(vector<vector<char>>& grid) {
-        int islands=0;
+        vis.resize(grid.size(),vector<bool>(grid[0].size()));
+        int island=0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[i].size();j++){
-                if(grid[i][j]=='1'){
-                    bfs(grid,i,j);
-                    islands++;
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    dfs(grid,i,j);
+                    island++;
                 }
             }
         }
-        return islands;
+        return island;
     }
 };
