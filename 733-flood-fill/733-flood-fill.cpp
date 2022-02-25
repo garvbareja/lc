c206234b7cfbc225831d5e9e348df715e074be2e
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-02-25 15:13:52 +0530

    Time: 11 ms (69.31%), Space: 13.7 MB (99.27%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    int color;
-    bool valid(vector<vector<int>>& image,int i,int j){
-        if(i<0 || i>=image.size() || j<0 || j>=image[0].size() || image[i][j]!=color) return false;
-        return true;
+    int og,nc;
+    void dfs(vector<vector<int>>& image,int i,int j){
+        if(i<0 || i>=image.size() || j<0 || j>=image[0].size() || image[i][j]!=og) return;
+            image[i][j]=nc;
+            dfs(image,i+1,j); dfs(image,i-1,j);
+            dfs(image,i,j+1); dfs(image,i,j-1);
     }
     
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        color=image[sr][sc];
-        queue<pair<int,int>> q; q.push({sr,sc});
-        image[sr][sc]=newColor;
-        if(color==newColor) return image;
-        vector<int> x = {-1,+1,0,0};
-        vector<int> y = {0,0,-1,+1};
-        while(!q.empty()){
-            pair<int,int> front = q.front(); q.pop();
-            for(int i=0;i<4;i++){
-                if(valid(image,front.first+x[i],front.second+y[i])){
-                    image[front.first+x[i]][front.second+y[i]] = newColor;
-                    q.push({front.first+x[i],front.second+y[i]});
-                }
-            }
-        }
+        og=image[sr][sc]; nc=newColor;
+        if(og!=nc) dfs(image,sr,sc);
         return image;
     }
 };
