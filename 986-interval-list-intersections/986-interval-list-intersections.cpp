ba266214fd6293c5d9d9508a88ba28d309367c78
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-02-25 15:58:20 +0530

    Time: 47 ms (50.35%), Space: 18.7 MB (45.82%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.cpp
+++ 986-interval-list-intersections/986-interval-list-intersections.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& f,vector<vector<int>>& s) {
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& f, vector<vector<int>>& s) {
         vector<vector<int>> ans;
-        int i=0,j=0;
+        int mn,mx; int i=0,j=0;
         while(i<f.size() && j<s.size()){
-            int low = max(f[i][0],s[j][0]);
-            int high = min(f[i][1],s[j][1]);
-            if(low<=high) ans.push_back({low,high});
+            mn=max(f[i][0],s[j][0]);
+            mx=min(f[i][1],s[j][1]);
+            if(mn<=mx) ans.push_back({mn,mx});
             f[i][1]<s[j][1]?i++:j++;
         }
         return ans;
