7b40b2c017498a9f66add7377a5a991f3d9a17eb
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-03-01 11:20:45 +0530

    Time: 110 ms (52.94%), Space: 30.4 MB (38.60%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
     bool valid(vector<vector<int>>& mat,int i,int j){
-        if(i<0 || i>=mat.size() || j<0 || j>=mat[i].size() || mat[i][j]==-1) return false;
+        if(i<0|| i>=mat.size() || j<0 || j>=mat[i].size() || !mat[i][j]) return false;
         return true;
     }
     
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        vector<vector<int>> res(mat.size(),vector<int>(mat[0].size()));
         queue<pair<int,int>> q;
-        vector<vector<int>> ans(mat.size(),vector<int>(mat[0].size()));
-        for(int i=0;i<mat.size();i++){
-            for(int j=0;j<mat[0].size();j++){
-                if(!mat[i][j]){
-                    mat[i][j]=-1;
-                    q.push({i,j});
-                }
-            }
-        }
+        for(int i=0;i<mat.size();i++)
+            for(int j=0;j<mat[0].size();j++)
+                if(!mat[i][j]) q.push({i,j});
         int steps=0;
         vector<int> x={-1,1,0,0};
         vector<int> y={0,0,1,-1};
         while(!q.empty()){
             int size=q.size();
             while(size--){
-                int i=q.front().first,j=q.front().second; q.pop();
-                ans[i][j]=steps;
-                for(int z=0;z<4;z++){
+                int i=q.front().first; int j=q.front().second; q.pop();
+                res[i][j]=steps;
+                for(int z=0;z<4;z++)
                     if(valid(mat,i+x[z],j+y[z])){
-                        mat[i+x[z]][j+y[z]]=-1;
                         q.push({i+x[z],j+y[z]});
+                        mat[i+x[z]][j+y[z]]=0;
                     }
-                }
             }
             steps++;
         }
-        return ans;
+        return res;
     }
 };
