2392cdf5704b10c44a8f7fcf76e7544c684ea629
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-03-12 15:55:50 +0530

    Time: 34 ms (8.46%), Space: 10 MB (87.44%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    static bool cmp(pair<int,int>&a, pair<int,int>& b){
-        if(a.first==b.first) return a.second<b.second;
-        return a.first<b.first;
-    }
-    int bits(int num){
-        int ans=0;
-        for(int i=31;i>=0;i--) if((1<<i)&num) ans++;
-        return ans;
+    static bool cmp(int& a,int& b){
+        int counta=0,countb=0;
+        for(int i=31;i>=0;i--){
+            if((1<<i)&a) counta+=1;
+            if((1<<i)&b) countb+=1;
+        }
+        if(counta==countb) return a<b;
+        return counta<countb;
     }
     
     vector<int> sortByBits(vector<int>& arr) {
-        vector<pair<int,int>> temp(arr.size());
-        for(int i=0;i<arr.size();i++) temp[i]={bits(arr[i]),arr[i]};
-        sort(temp.begin(),temp.end(),cmp);
-        for(int i=0;i<arr.size();i++) arr[i]=temp[i].second;
+        sort(arr.begin(),arr.end(),cmp);
         return arr;
     }
 };
