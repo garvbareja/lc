caca2423ed6782cbb33757485cec18ee80d590ec
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-03-13 10:08:50 +0530

    Time: 6 ms (11.70%), Space: 6.1 MB (96.29%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isValid(string s) {
+        if(s.length()%2) return false;
         stack<char> stk;
         for(int i=0;i<s.length();i++){
             if(s[i]=='(' || s[i]=='{' || s[i]=='[') stk.push(s[i]);
