34d271220676a66a598d87b70773995c7212a50e
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-03-17 19:09:06 +0530

    Time: 3 ms (99.71%), Space: 10.9 MB (87.09%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
-    int numOfPairs(vector<string>& nums, string &t){
-        int res=0;
+    int numOfPairs(vector<string>& nums, string &t) {
+        unordered_map<string,int> hash;
+        int res=0; string substring;
+        for(int i=0;i<nums.size();i++) hash[nums[i]]++;
         for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums.size();j++){
-                if(i==j) continue;
-                if(nums[i]+nums[j]==t) res++;
+            if(nums[i].size()<t.length()){
+                if(nums[i][0]!=t[0]) continue;
+                substring=t.substr(nums[i].size(),t.length()-1);
+                if(hash.count(substring) && nums[i]+substring==t){
+                    if(nums[i]==substring) res+=hash[substring]-1;
+                    else res+=hash[substring];
+                }
             }
         }
         return res;
