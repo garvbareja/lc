a99f5029084fa80aa2e3e85f8e79eddd3744ca15
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-06-14 12:29:13 +0530

    Time: 45 ms (73.99%), Space: 19.1 MB (66.70%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    static bool custom(vector<int>& a,vector<int>& b){
+    static bool custom(vector<int>&a, vector<int>& b){
         return a[0]<b[0];
     }
     
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),custom);
-        vector<vector<int>> res; res.push_back(intervals[0]);
-        for(int i=1;i<intervals.size();i++){
-            if(res.back()[1]>=intervals[i][0]) res.back()[1]=max(res.back()[1],intervals[i][1]);
-            else res.push_back(intervals[i]);
+    vector<vector<int>> merge(vector<vector<int>>& nums) {
+        sort(nums.begin(),nums.end(),custom);
+        vector<vector<int>> res; res.push_back(nums[0]); int i=1;
+        while(i<nums.size()){
+            if(nums[i][0]<=res.back()[1]) res.back()[1]=max(res.back()[1],nums[i][1]);
+            else res.push_back(nums[i]);
+            i++;
         }
         return res;
     }
