f825b33df1c23a4fce5bc762fedc4cc9a679ca53
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-06-16 15:29:51 +0530

    Time: 29 ms (19.38%), Space: 9.8 MB (94.45%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>& nums,int i,int j,vector<vector<int>>& dp){
-        if(i>=nums.size() || j>=nums[0].size()) return INT_MAX;
-        if(i==nums.size()-1) return nums[i][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=nums[i][j]+min(min(dfs(nums,i+1,j-1,dp),dfs(nums,i+1,j,dp)),dfs(nums,i+1,j+1,dp));
-    }
-    
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int ans=INT_MAX;
-        vector<vector<int>> dp(matrix.size(),vector<int>(matrix[0].size(),-1));
-        for(int j=0;j<matrix[0].size();j++) ans=min(ans,dfs(matrix,0,j,dp));
-        return ans;
+        vector<int> prev(matrix.back().begin(),matrix.back().end());
+        vector<int> cur(matrix[0].size());
+        for(int i=matrix.size()-2;i>=0;i--){
+            for(int j=matrix[i].size()-1;j>=0;j--){
+                cur[j]=prev[j];
+                if(j-1>=0) cur[j]=min(cur[j],prev[j-1]);
+                if(j+1<matrix[i].size()) cur[j]=min(cur[j],prev[j+1]);
+                cur[j]+=matrix[i][j];
+            }
+            prev=cur;
+        }
+        return *min_element(prev.begin(),prev.end());
     }
 };
