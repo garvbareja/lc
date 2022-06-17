874ea03fa5207a48f8f77a8d0c2f6a2d3d051cb2
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-06-17 16:09:22 +0530

    Time: 65 ms (24.97%), Space: 18.6 MB (34.26%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    int dfs(vector<int>& coins,int target,int n,vector<vector<int>>& dp){
-        if(!target) return 1;
-        if(!n) return 0;
-        if(dp[n][target]!=-1) return dp[n][target];
-        int take=0;
-        if(coins[n-1]<=target) take=dfs(coins,target-coins[n-1],n,dp);
-        int leave=dfs(coins,target,n-1,dp);
-        return dp[n][target]=take+leave;
-    }
     
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,-1));
-        return dfs(coins,amount,coins.size(),dp);
+        vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,0));
+        for(int i=0;i<=coins.size();i++) dp[i][0]=1;
+        for(int i=1;i<=coins.size();i++){
+            for(int j=1;j<=amount;j++){
+                dp[i][j]=dp[i-1][j];
+                if(coins[i-1]<=j) dp[i][j]+=dp[i][j-coins[i-1]];
+            }
+        }
+        return dp[coins.size()][amount];
     }
 };
