ce114c0a41534ee574ca6e182f31c7dcf324eab6
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-06-20 17:14:05 +0530

    Time: 20 ms (44.55%), Space: 13.2 MB (24.47%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    int dfs(vector<int>& nums,int index,int state,int k,vector<vector<vector<int>>>& dp){
-        if(index>=nums.size()) return 0;
-        if(!k) return 0;
-        if(dp[index][state][k]!=-1) return dp[index][state][k];
-        if(state){
-            return dp[index][state][k]=max(dfs(nums,index+1,1,k,dp),-nums[index]+dfs(nums,index+1,0,k,dp));
-        }else{
-            return dp[index][state][k]=max(dfs(nums,index+1,0,k,dp),nums[index]+dfs(nums,index+1,1,k-1,dp));
+    int maxProfit(int kk, vector<int>& nums) {
+        vector<vector<vector<int>>> dp(nums.size()+1,vector<vector<int>>(2,vector<int>(kk+1,0)));
+        for(int index=nums.size()-1;index>=0;index--){
+                for(int k=1;k<=kk;k++){
+                    dp[index][1][k]=max(dp[index+1][1][k],-nums[index]+dp[index+1][0][k]);
+                    dp[index][0][k]=max(dp[index+1][0][k],nums[index]+dp[index+1][1][k-1]);
+                }
         }
-    }
-    
-    int maxProfit(int k, vector<int>& prices) {
-        vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>>(2,vector<int>(k+1,-1)));
-        return dfs(prices,0,1,k,dp);
+        return dp[0][1][kk];
     }
 };
