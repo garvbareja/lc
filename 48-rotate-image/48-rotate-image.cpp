9bdb1085e75cbd3d8f05132e07b85d26275ee16c
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-06-22 12:47:56 +0530

    Time: 3 ms (61.80%), Space: 7.2 MB (33.36%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& nums) {
-        int left=0,top=0,right=nums.size()-1,bottom=nums.size()-1;
-        while(left<right && top<bottom){
+        int top=0,left=0,right=nums.size()-1,bottom=nums.size()-1;
+        while(top<bottom && left<right){
             for(int i=0;i<right-left;i++){
-                int topleft=nums[top][left+i];
+                int topleftval=nums[top][left+i];
                 nums[top][left+i]=nums[bottom-i][left];
                 nums[bottom-i][left]=nums[bottom][right-i];
                 nums[bottom][right-i]=nums[top+i][right];
-                nums[top+i][right]=topleft;
+                nums[top+i][right]=topleftval;
             }
-            top++; bottom--;
-            right--; left++;
+            left++; right--; top++; bottom--;
         }
     }
 };
