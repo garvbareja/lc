f1ad60f18e476d45a46b812925f07d2a0526ec00
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-07-04 21:30:45 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (80.08%) - LeetHub

--- 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int minMutation(string& start, string& end, vector<string>& bank) {
-        unordered_set<string> vis; queue<string> q; q.push(start); vis.insert(start);
-        string temp=""; vector<char> gene={'A','C','G','T'}; unordered_set<string> hash;
-        for(int i=0;i<bank.size();i++) hash.insert(bank[i]);
-        int res=0; char og;
+        queue<string> q; q.push(start); unordered_set<string> hash;
+        vector<char> gene={'A','C','G','T'};
+        for(int i=0;i<bank.size();i++) hash.insert(bank[i]); hash.erase(start);
+        int res=0; char og; string temp="";
         while(!q.empty()){
             int size=q.size();
             while(size--){
@@ -14,8 +14,8 @@ public:
                     for(int j=0;j<4;j++){
                         og=temp[i];
                         temp[i]=gene[j];
-                        if(!vis.count(temp) && hash.count(temp)){
-                            vis.insert(temp);
+                        if(hash.count(temp)){
+                            hash.erase(temp);
                             q.push(temp);
                         }
                         temp[i]=og;
