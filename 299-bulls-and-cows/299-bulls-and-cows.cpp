1ff21d2a3eb9d3b2d74750b018ac0f0597a5d3a7
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-07-04 13:44:09 +0530

    Time: 15 ms (11.33%), Space: 6.5 MB (94.22%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.cpp
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     string getHint(string& s, string& g) {
-        unordered_map<char,unordered_set<int>> hash; int bulls=0,cows=0;
-        for(int i=0;i<s.length();i++) hash[s[i]].insert(i);
-        for(int i=0;i<g.length();i++){
-            if(hash.count(g[i]) && hash[g[i]].count(i)){
-                bulls++; hash[g[i]].erase(i);
-                if(hash[g[i]].size()==0) hash.erase(g[i]);
-            }
+        unordered_map<char,int> hash; int bulls=0,cows=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]==g[i]) bulls++;
+            else hash[s[i]]++;
         }
         for(int i=0;i<g.length();i++){
-            cout<<hash.count('0');
-            if(hash.count(g[i]) && s[i]!=g[i]){
-                cows++; hash[g[i]].erase(hash[g[i]].begin());
-                if(hash[g[i]].size()==0) hash.erase(g[i]);
+            if(s[i]!=g[i] && hash.count(g[i])){
+                hash[g[i]]--;  cows++;
+                if(hash[g[i]]==0) hash.erase(g[i]);
             }
         }
         return to_string(bulls)+"A"+to_string(cows)+"B";
