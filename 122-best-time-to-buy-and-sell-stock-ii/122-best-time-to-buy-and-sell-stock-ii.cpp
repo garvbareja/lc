343432261e77a3f42d61397168e464ff2aeb5b71
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-07-05 03:01:13 +0530

    Time: 15 ms (30.45%), Space: 15.2 MB (14.08%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
-//     int dfs(vector<int>& nums,int n,int price,vector<vector<int>>& dp){
-//         if(!n) return 0;
-//         if(dp[n][price+1]!=-1) return dp[n][price+1];
-//         if(price>=0){
-//             return dp[n][price+1]=max(dfs(nums,n-1,price,dp),price-nums[n-1]+dfs(nums,n-1,-1,dp));
-//         }else{
-//             return dp[n][price+1]=max(dfs(nums,n-1,-1,dp),dfs(nums,n-1,nums[n-1],dp));
-//         }
-//     }
-    
-    
-//     int maxProfit(vector<int>& nums) {
-//         int size=*max_element(nums.begin(),nums.end())+3;
-//         vector<vector<int>> dp(nums.size()+1,vector<int>(size,-1));
-//         return dfs(nums,nums.size(),-1,dp);
-//     }
-    int dfs(vector<int>& nums,int n,int cansell,vector<vector<int>>& dp){
-        if(!n) return 0;
-        if(n==1){
-            if(cansell) return 0;
-            else return -nums[0];
-        }
-        if(dp[n][cansell]!=-1) return dp[n][cansell];
-        if(cansell) return dp[n][cansell]=max(dfs(nums,n-1,1,dp),nums[n-1]+dfs(nums,n-1,0,dp));
-        else return dp[n][cansell]=max(dfs(nums,n-1,0,dp),dfs(nums,n-1,1,dp)-nums[n-1]);
+    int dfs(vector<int>& nums,int i,int state,vector<vector<int>>& dp){
+        if(i>=nums.size()) return 0;
+        if(dp[i][state]!=-1) return dp[i][state];
+        if(state) return dp[i][state] = max(dfs(nums,i+1,1,dp),-nums[i]+dfs(nums,i+1,0,dp));
+        else return dp[i][state] = max(dfs(nums,i+1,0,dp),nums[i]+dfs(nums,i+1,1,dp));
     }
     
-    
     int maxProfit(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size()+1,vector<int>(2,-1));
-        return dfs(nums,nums.size(),1,dp);
+        vector<vector<int>> dp(nums.size()+1,vector<int>(2));
+        for(int i=nums.size()-1;i>=0;i--){
+            dp[i][1]=max(dp[i+1][1],-nums[i]+dp[i+1][0]);
+            dp[i][0]=max(dp[i+1][0],nums[i]+dp[i+1][1]);
+        }
+        return dp[0][1];
     }
 };
