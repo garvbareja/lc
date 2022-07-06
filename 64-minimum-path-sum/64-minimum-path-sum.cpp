216c16e39b035c03434028d5b459caaf493eed28
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-07-06 22:08:08 +0530

    Time: 16 ms (44.18%), Space: 10.2 MB (6.00%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,12 +1,5 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>& grid,int i,int j,vector<vector<int>>& dp){
-        if(i==grid.size()-1 && j==grid[0].size()-1) return grid[i][j];
-        if(i>=grid.size() || j>=grid[0].size()) return INT_MAX;
-        if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=grid[i][j]+min(dfs(grid,i+1,j,dp),dfs(grid,i,j+1,dp));
-    }
-    
     int minPathSum(vector<vector<int>>& grid) {
         vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size()));
         for(int i=grid.size()-1;i>=0;i--){
