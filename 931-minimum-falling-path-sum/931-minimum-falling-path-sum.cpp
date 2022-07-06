63953214c6bd85a227a61409d75f26a2e1210448
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-07-06 20:48:20 +0530

    Time: 34 ms (12.48%), Space: 9.7 MB (95.26%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int ans=INT_MAX;
-        vector<int> prev(matrix.back().begin(),matrix.back().end());
-        vector<int> cur(matrix[0].size());
-        for(int i=matrix.size()-2;i>=0;i--){
-            for(int j=matrix[i].size()-1;j>=0;j--){
+    int minFallingPathSum(vector<vector<int>>& nums) {
+        int res=INT_MAX;
+        vector<int> prev(nums.back().begin(),nums.back().end()); vector<int> cur(nums[0].size());
+        for(int i=nums.size()-2;i>=0;i--){
+            for(int j=nums[0].size()-1;j>=0;j--){
                 cur[j]=prev[j];
                 if(j-1>=0) cur[j]=min(cur[j],prev[j-1]);
-                if(j+1<matrix[i].size()) cur[j]=min(cur[j],prev[j+1]);
-                cur[j]+=matrix[i][j];
+                if(j+1<nums[0].size()) cur[j]=min(cur[j],prev[j+1]);
+                cur[j]+=nums[i][j];
+                if(!i) res=min(res,cur[j]);
             }
             prev=cur;
         }
