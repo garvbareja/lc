2ed7ac1c0b3b68f8d51984eea7e7ad94f0b59e6a
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-07-07 15:26:09 +0530

    Time: 11 ms (19.53%), Space: 11.1 MB (74.87%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int bs_smallest(vector<int>& nums) {
-        int low=0,high=nums.size()-1,left,right,mid;
+    int findMin(vector<int>& nums) {
+        int low=0,high=nums.size()-1,mid,left,right;
         while(low<=high){
             mid=low+(high-low)/2;
             if(mid-1>=0) left=nums[mid-1];
             else left=INT_MAX;
             if(mid+1<nums.size()) right=nums[mid+1];
             else right=INT_MAX;
-            if(nums[mid]<left && nums[mid]<right) return mid;
+            if(left>nums[mid] && right>nums[mid]) return mid;
             if(nums[low]>nums[mid]) high=mid-1;
             else if(nums[high]<nums[mid]) low=mid+1;
             else return low;
@@ -21,17 +21,16 @@ public:
         while(low<=high){
             mid=low+(high-low)/2;
             if(nums[mid]==target) return mid;
-            if(nums[mid]<target) low++;
-            else high--;
+            if(nums[mid]<target) low=mid+1;
+            else high=mid-1;
         }
         return -1;
     }
     
     int search(vector<int>& nums, int target) {
-        int index=bs_smallest(nums),res;
-        
-        res=bs(nums,0,index-1,target); if(res!=-1) return res;
-        res=bs(nums,index,nums.size()-1,target); if(res!=-1) return res;
-        return -1;
+        int index=findMin(nums);
+        int res=bs(nums,0,index-1,target);
+        if(res==-1) res=bs(nums,index,nums.size()-1,target);
+        return res;
     }
 };
