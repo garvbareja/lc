8863960712aebcefe987fb9e7b134334e61f7407
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-07-14 17:34:20 +0530

    Time: 15 ms (5.15%), Space: 11 MB (75.00%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
-        int low=0,high=nums.size()-1,mid,left,right;
+    int find_pivot(vector<int>& nums){
+        int low=0,high=nums.size()-1,mid,res;
         while(low<=high){
             mid=low+(high-low)/2;
+            int left=INT_MAX;
             if(mid-1>=0) left=nums[mid-1];
-            else left=INT_MAX;
+            int right=INT_MAX;
             if(mid+1<nums.size()) right=nums[mid+1];
-            else right=INT_MAX;
-            if(left>nums[mid] && right>nums[mid]) return mid;
+            if(nums[mid]<left && nums[mid]<right) return mid;
             if(nums[low]>nums[mid]) high=mid-1;
             else if(nums[high]<nums[mid]) low=mid+1;
             else return low;
         }
-        return -1;
+        return 0;
     }
     
-    int bs(vector<int>& nums,int low,int high,int target){
+    int bs(vector<int>& nums,int target,int low,int high){
         int mid;
         while(low<=high){
             mid=low+(high-low)/2;
@@ -28,9 +28,9 @@ public:
     }
     
     int search(vector<int>& nums, int target) {
-        int index=findMin(nums);
-        int res=bs(nums,0,index-1,target);
-        if(res==-1) res=bs(nums,index,nums.size()-1,target);
+        int pivot=find_pivot(nums);
+        int res=bs(nums,target,0,pivot-1);
+        if(res==-1) res=bs(nums,target,pivot,nums.size()-1);
         return res;
     }
 };
