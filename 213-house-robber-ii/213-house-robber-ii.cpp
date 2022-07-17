31949db72d7e23999a99b179d8cad9e3bd442801
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-07-17 22:55:08 +0530

    Time: 3 ms (58.27%), Space: 7.7 MB (84.94%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int dfs(vector<int>& nums,int left,int right) {
-        int prev=0,dprev=0,cur;
+    int dfs(vector<int>& nums,int left,int right){
+        int prev=0,prev2=0;
         for(int i=right;i>=left;i--){
-            cur=max(nums[i]+dprev,prev);
-            dprev=prev;
+            int cur=max(prev,nums[i]+prev2);
+            prev2=prev;
             prev=cur;
         }
-        return prev;
+        return max(prev,prev2);
     }
     
     int rob(vector<int>& nums) {
         if(nums.size()==1) return nums[0];
-        return max(dfs(nums,0,nums.size()-2),dfs(nums,1,nums.size()-1));
+        else return max(dfs(nums,0,nums.size()-2),dfs(nums,1,nums.size()-1));
     }
 };
