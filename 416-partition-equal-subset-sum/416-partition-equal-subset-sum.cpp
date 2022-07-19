5cfe75232c7e60be3eafdb36dd97e2c9105693cf
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-07-19 23:48:55 +0530

    Time: 430 ms (30.91%), Space: 11.9 MB (64.44%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    bool subsetSumToK(int n, int k, vector<int> &arr) {
-    vector<vector<bool>> dp(arr.size()+1,vector<bool>(k+1,false));
-    for(int i=0;i<arr.size();i++){
-        dp[i][0]=true;
-    }
-    for(int i=1;i<=arr.size();i++){
-        for(int j=1;j<=k;j++){
-            dp[i][j]=dp[i-1][j];
-            if(arr[i-1]<=j) if(!dp[i][j]) dp[i][j]=dp[i-1][j-arr[i-1]];
-        }
-    }
-    return dp.back().back();
-}
-    
     bool canPartition(vector<int>& nums) {
-        int sum=0;
+        int sum=0; bool leave=false,take=false;
         for(int i=0;i<nums.size();i++) sum+=nums[i];
         if(sum%2) return false;
-        return subsetSumToK(nums.size(),sum/2,nums);
+        vector<vector<bool>> dp(nums.size()+1,vector<bool>((sum/2)+1,false));
+        for(int n=0;n<=nums.size();n++){
+            for(int target=0;target<=(sum/2);target++){
+                if(!target) dp[n][target]=true;
+                else if(!n) dp[n][target]=false;
+                else{
+                    take = false;
+                    if(nums[n-1]<=target) take=dp[n-1][target-nums[n-1]];
+                    leave=dp[n-1][target];
+                    dp[n][target]=leave||take;
+                }
+            }
+        }
+        return dp[nums.size()][sum/2];
     }
 };
