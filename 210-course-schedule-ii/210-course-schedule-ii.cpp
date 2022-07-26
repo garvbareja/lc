20f876a7a34a8218c5badfc7ab24450aab2ec431
garvbareja <54734399+garvbareja@users.noreply.github.com>
2022-07-27 01:40:52 +0530

    Time: 32 ms (56.92%), Space: 13.8 MB (70.43%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> res;
+    bool check_cycle(vector<int> adj[],int v,vector<bool>& vis,vector<bool>& cycle){
+        vis[v]=true; cycle[v]=true;
+        for(int i=0;i<adj[v].size();i++){
+            if(!vis[adj[v][i]]){
+                if(check_cycle(adj,adj[v][i],vis,cycle)) return true;
+            }else if(cycle[adj[v][i]]) return true;
+        }
+        cycle[v]=false;
+        res.push_back(v);
+        return false;
+    }
+    
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+        vector<int> adj[n]; vector<bool> cycle(n),vis(n);
+        for(int i=0;i<prerequisites.size();i++) adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        for(int v=0;v<n;v++) if(!vis[v]) if(check_cycle(adj,v,vis,cycle)) return {};
+        return res;
+    }
+};
